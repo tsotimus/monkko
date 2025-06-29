cd8cb54259826b11454129217a3bec0b5ed8f1aa
Tsotne Gvadzabia <32150283+tsotimus@users.noreply.github.com>
2025-06-29 21:56:06 +0100

    cleanup test utils

--- packages/cli-testing/src/utils/getExpectedFiles.ts
+++
@@ -1,8 +0,0 @@
-import { glob } from 'glob';
-
-/**
- * Get all mock files that should have corresponding generated files
- */
-export async function getExpectedFiles(mockDir: string): Promise<string[]> {
-  return await glob('**/*.ts', { cwd: mockDir });
-} 
--- packages/cli-testing/src/utils/index.ts
+++ packages/cli-testing/src/utils/index.ts
@@ -1,8 +1,5 @@
 export { compareFile } from './compareFile';
 export type { FileComparisonResult } from './compareFile';
-export { compareAllFiles } from './compareAllFiles';
-export { fileExists } from './fileExists';
-export { getExpectedFiles } from './getExpectedFiles';
 export { normalizeContent } from './normalizeContent';
 export { 
   extractSchemaNames, 
